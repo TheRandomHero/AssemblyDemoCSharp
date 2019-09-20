e6a1c93da20f7bd714e871bfb31c9987dedadb96
Attila Molnar <scythianhawk@gmai.com>
2019-09-20 11:36:27 +0200

    Second Exercise done

---
+++ AssemblyAttrDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            Type descrioptionType = typeof(AssemblyDescriptionAttribute);
+
+            object[] attributes = currentAssembly.GetCustomAttributes(descrioptionType, false);
+
+            if (attributes.Length > 0)
+            {
+                AssemblyDescriptionAttribute newDescription = (AssemblyDescriptionAttribute)attributes[0];
+                Console.WriteLine("Description: {0}", newDescription.Description);
+            }
+        }
+    }
+}
